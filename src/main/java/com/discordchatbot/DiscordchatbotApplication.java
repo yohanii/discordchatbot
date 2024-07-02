a942c9d7a3fec8e14802d6d69df04e98500ab2cc
yohan <dygks924@gmail.com>
2024-07-02 12:50:05 +0900

    first commit

---
+++ src/main/java/com/discordchatbot/DiscordchatbotApplication.java
@@ -0,0 +1,13 @@
+package com.discordchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordchatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordchatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discordchatbot/DiscordchatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discordchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordchatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
