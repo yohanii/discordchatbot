63f0f298fcff1126405bf537fe92b2fcbcdf639f
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-07-31 15:50:48 +0900

    test: QuotesService 생성자로 변경 및 테스트 추가

--- src/main/java/com/discordchatbot/feign/response/QuoteResponse.java
+++ src/main/java/com/discordchatbot/feign/response/QuoteResponse.java
@@ -1,8 +1,12 @@
 package com.discordchatbot.feign.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
 public class QuoteResponse {
 
     private String quote;
--- src/main/java/com/discordchatbot/feign/response/QuotesResponse.java
+++ src/main/java/com/discordchatbot/feign/response/QuotesResponse.java
@@ -1,10 +1,14 @@
 package com.discordchatbot.feign.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
+@AllArgsConstructor
 public class QuotesResponse {
 
     private List<QuoteResponse> quotes;
